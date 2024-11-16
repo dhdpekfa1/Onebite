ce41a85980815203b61ccbe3a0b1566e6c785fbb
닮 <dhdpekfa1@daum.net>
2024-11-16 17:19:30 +0900

    [navigating]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/test"); // push('/이동할 경로') 작성
+  };
+
+  return (
+    <>
+      <header>
+        {/* CSL - Link Component 사용 */}
+        <Link href={"/"}>Home</Link>
+        &nbsp;
+        <Link href={"/book"}>book</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        {/* Programmatic Navigation */}
+        <div>
+          <button onClick={handleClick}>/test Page로 이동!</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
