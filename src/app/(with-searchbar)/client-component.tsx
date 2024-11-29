51d3f2247c3f87c0153c06e92c6ff56b24f80b0b
닮 <dhdpekfa1@daum.net>
2024-11-29 21:52:01 +0900

    [AppRouter 시작 및 이해]

--- src/app/(with-searchbar)/client-component.tsx
+++ src/app/(with-searchbar)/client-component.tsx
@@ -4,6 +4,11 @@ import { ReactNode } from "react";
 const ClientComponent = ({ children }: { children: ReactNode }) => {
   console.log("클라이언트 컴포넌트");
   return <div>{children}</div>;
+  /** 다른 컴포넌트에서 사용 시 ex)home
+   * <ClientComponent>
+   *   <ServerComponent/>
+   *</ClientComponent>
+   *  */
 };
 
 export default ClientComponent;
