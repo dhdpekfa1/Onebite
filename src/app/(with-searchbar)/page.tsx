88e64ea92964b86cfc1d615e877f2b7f15e3c30e
닮 <dhdpekfa1@daum.net>
2024-11-30 16:14:04 +0900

    [Data Fetching]

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,55 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
+/** component를 각각 AllBooks, RecoBooks로 분리
+ *  -> fetching 각각 한 번만 실행*/
+const AllBooks = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!res.ok) {
+    return <div>데이터를 불러오는 중 오류가 발생했습니다..</div>;
+  }
+  const allBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+const RecoBooks = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+  if (!res.ok) {
+    return <div>데이터를 불러오는 중 오류가 발생했습니다..</div>;
+  }
+  const recoBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+/** 한 컴포넌트에서 여러 개의  date fetching이 일어나는 경우
+ *  컴포넌트 분리 후 import, 해당 component 사용 */
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,24 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
+  // Page component에 자동으로 props로 제공되는 현재 페이지의 query string
   searchParams,
 }: {
   searchParams: {
     q?: string;
   };
 }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+  );
+  if (!res.ok) {
+    return <div>데이터를 불러오는 중 오류가 발생했습니다..</div>;
+  }
+  const searchBooks: BookData[] = await res.json();
   return (
     <div>
-      {books.map((book) => (
+      {searchBooks.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
