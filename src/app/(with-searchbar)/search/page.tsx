9aac503a73cbf8433913376ddd9eb37fc793623a
닮 <dhdpekfa1@daum.net>
2024-12-03 21:43:31 +0900

    [FullRouteCache_Dynamic]

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -2,15 +2,16 @@ import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
 export default async function Page({
-  // Page component에 자동으로 props로 제공되는 현재 페이지의 query string
   searchParams,
 }: {
-  searchParams: {
-    q?: string;
-  };
+  searchParams: { q?: string };
 }) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+    // 검색어로부터 query를 불러와서 사용하는 동적 경로
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    // 페이지는 계속 다시 생성되지만 검색 결과는 캐싱 가능
+    // -> 한 번 검색한 검색어의 경우 조금 더 빠른 데이터 응답 가능
+    { cache: "force-cache" }
   );
   if (!res.ok) {
     return <div>데이터를 불러오는 중 오류가 발생했습니다..</div>;
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,15 +1,34 @@
 import { BookData } from "@/types";
 import style from "./page.module.css";
+import { notFound } from "next/navigation";
+
+// generateStaticParams에서 반환환 경로 외 동적 경로 생성x -> 404
+// export const dynamicParams = false;
+
+/** 정적인 parameter를 생성하는 함수(export 필수)
+ *  -> 아래 Page 컴포넌트 내부에 데이터 캐시 설정이 없다라도 해당 함수를 사용 시
+ *     해당 페이지는 무조건 static page로 강제 설정 */
+export const generateStaticParams = () => {
+  // 존재할 수 있는 동적 경로(url parameter)를 문자열로 작성
+  // -> 빌드 타임에 해당 parameter를 읽고 parameter에 해당하는 페이지를 정적 생성
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+  // -> 다른 id로 접근 시 실시간 요청, 생성되어 Full Route Cache로 저장
+};
 
 export default async function Page({
-  params, // Page component에 자동으로 props로 전달되는 Url parameter 값
+  params,
 }: {
   params: { id: string | string[] };
 }) {
+  // generateStaticParams 함수를 사용했기 때문에 별도의 데이터 캐시 설정 없어도 1, 2, 3 페이지는 정적 생성
   const res = await fetch(
+    //
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`
   );
   if (!res.ok) {
+    if (res.status === 404) {
+      notFound();
+    }
     return <div>데이터를 불러오는 중 오류가 발생했습니다..</div>;
   }
   const book: BookData = await res.json();
